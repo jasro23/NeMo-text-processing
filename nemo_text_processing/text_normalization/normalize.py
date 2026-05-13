7fe074502f6c2791a0134e99687f2d042f6181dd
Jason Roche <jroche@nvidia.com>
2026-05-13 12:11:15 -0700

    Register Kannada (kn) in Normalizer API

--- nemo_text_processing/text_normalization/normalize.py
+++ nemo_text_processing/text_normalization/normalize.py
@@ -191,6 +191,9 @@ class Normalizer:
         elif lang == 'ko':
             from nemo_text_processing.text_normalization.ko.taggers.tokenize_and_classify import ClassifyFst
             from nemo_text_processing.text_normalization.ko.verbalizers.verbalize_final import VerbalizeFinalFst
+        elif lang == 'kn':
+            from nemo_text_processing.text_normalization.kn.taggers.tokenize_and_classify import ClassifyFst
+            from nemo_text_processing.text_normalization.kn.verbalizers.verbalize_final import VerbalizeFinalFst
         else:
             raise NotImplementedError(f"Language {lang} has not been supported yet.")
 
